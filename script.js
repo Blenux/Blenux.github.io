ed6b9730adc6cbf7bd94aed2ced951fcb1089392
Blenux <juso3d@gmail.com>
2023-05-30 18:32:37 +1000

    js default theme

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ var themeStyle = document.getElementById('theme-style');
 
 // Check if a theme preference is stored in local storage
 var selectedTheme = localStorage.getItem('theme');
-if (selectedTheme) {
-  themeStyle.setAttribute('href', selectedTheme);
+
+if (selectedTheme === 'dark.css') {
+  // If the last selected theme was dark, set it as the current theme
+  themeStyle.setAttribute('href', 'dark.css');
+} else {
+  // Otherwise, set the default theme (light.css)
+  themeStyle.setAttribute('href', 'light.css');
 }
 
 toggleButton.addEventListener('click', function() {
